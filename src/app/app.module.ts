bd380889b0df1b1b3283abb2729f6aeda3f5e8e8
Javier <2190271@my.ipleiria.pt>
2020-03-29 15:50:04 +0100

    #1 - Add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { HeaderComponent } from "./header/header.component";
 import { TransactionsListComponent } from "./transactions-list/transactions-list.component";
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TransactionsListComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, TransactionsListComponent, HomeComponent, ChartComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
