8ffb74e453b09c908f0f659839b16facc549a1db
Javier <2190271@my.ipleiria.pt>
2020-04-20 18:56:01 +0100

    Add kendo lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { TransactionsListComponent } from "./transactions-list/transactions-list.component";
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { ChartComponent } from './chart/chart.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { ChartComponent } from "./chart/chart.component";
+import { HttpClientModule } from "@angular/common/http";
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TransactionsListComponent, HomeComponent, ChartComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TransactionsListComponent,
+    HomeComponent,
+    ChartComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, GridModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
