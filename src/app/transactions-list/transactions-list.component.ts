6d9ce6c330150569b0cb49e895180e58db09c32c
Javier <2190271@my.ipleiria.pt>
2020-04-20 19:01:25 +0100

    Add transaction grid

--- src/app/transactions-list/transactions-list.component.ts
+++ src/app/transactions-list/transactions-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TransactionsService } from "../services/transactions.service";
+import { TransactionModel } from "../models/transaction-model";
 
 @Component({
-  selector: 'app-transactions-list',
-  templateUrl: './transactions-list.component.html',
-  styleUrls: ['./transactions-list.component.scss']
+  selector: "app-transactions-list",
+  templateUrl: "./transactions-list.component.html",
+  styleUrls: ["./transactions-list.component.scss"]
 })
 export class TransactionsListComponent implements OnInit {
+  transactions: TransactionModel[];
 
-  constructor() { }
+  constructor(private transactionService: TransactionsService) {}
 
   ngOnInit() {
+    this.transactionService.getTransactions().subscribe(
+      result => this.onGetTransactionsSuccess(result),
+      error => this.onGetTransactionsError(error)
+    );
   }
 
+  onGetTransactionsSuccess(result: TransactionModel[]) {
+    let gridData = result.map(item => {
+      item.createdAt = new Date(item.createdAt);
+      return item;
+    });
+    this.transactions = gridData;
+  }
+
+  onGetTransactionsError(error) {}
 }
