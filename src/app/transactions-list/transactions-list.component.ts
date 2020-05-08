d178abffce3433782603832bb6d6eb5ac59028e8
Javier <2190271@my.ipleiria.pt>
2020-05-08 16:05:46 +0100

    Refactor classification list

--- src/app/transactions-list/transactions-list.component.ts
+++ src/app/transactions-list/transactions-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { TransactionsService } from "../services/transactions.service";
 import { TransactionModel } from "../models/transaction-model";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-transactions-list",
@@ -9,10 +10,12 @@ import { TransactionModel } from "../models/transaction-model";
 })
 export class TransactionsListComponent implements OnInit {
   transactions: TransactionModel[];
+  classificationTypes = [];
 
   constructor(private transactionService: TransactionsService) {}
 
   ngOnInit() {
+    this.initClassification();
     this.transactionService.transactions.subscribe(
       (result) => this.onGetTransactionsSuccess(result),
       (error) => this.onGetTransactionsError(error)
@@ -28,5 +31,20 @@ export class TransactionsListComponent implements OnInit {
   onClickClassifyBtn(rowIndex: number, classification: string) {
     this.transactions[rowIndex].classification = classification;
     this.transactionService.setTransactions(this.transactions);
+  }
+
+  initClassification() {
+    var classificationTypes = [
+      { name: "food", icon: faCoffee },
+      { name: "health", icon: faCoffee },
+      { name: "education", icon: faCoffee },
+      { name: "household", icon: faCoffee },
+      { name: "entertainment", icon: faCoffee },
+      { name: "transportation", icon: faCoffee },
+      { name: "vet", icon: faCoffee },
+      { name: "others", icon: faCoffee },
+    ];
+
+    this.classificationTypes = classificationTypes;
   }
 }
