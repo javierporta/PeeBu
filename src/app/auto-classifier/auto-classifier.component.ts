564225d5c1bad1257448e75298ee3c08ceba3cd7
Javier <2190271@my.ipleiria.pt>
2020-05-14 20:24:27 +0100

    Add transaction service in auto classifier component

--- src/app/auto-classifier/auto-classifier.component.ts
+++ src/app/auto-classifier/auto-classifier.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TransactionsService } from "../services/transactions.service";
+import { TransactionModel } from "../models/transaction-model";
 
 @Component({
-  selector: 'app-auto-classifier',
-  templateUrl: './auto-classifier.component.html',
-  styleUrls: ['./auto-classifier.component.scss']
+  selector: "app-auto-classifier",
+  templateUrl: "./auto-classifier.component.html",
+  styleUrls: ["./auto-classifier.component.scss"],
 })
 export class AutoClassifierComponent implements OnInit {
+  transactions: TransactionModel[];
 
-  constructor() { }
+  constructor(private transactionService: TransactionsService) {}
 
   ngOnInit() {
+    this.transactionService.transactions.subscribe(
+      (result) => this.onGetTransactionsSuccess(result),
+      (error) => this.onGetTransactionsError(error)
+    );
   }
 
+  onGetTransactionsSuccess(result: TransactionModel[]) {
+    this.transactions = result;
+  }
+
+  onGetTransactionsError(error) {}
 }
