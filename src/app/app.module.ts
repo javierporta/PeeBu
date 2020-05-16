9b8d26b16c87d718cc073b59c385e8d4099878e3
Javier <2190271@my.ipleiria.pt>
2020-05-16 18:56:34 +0100

    Remove auto classifier component as it wont be used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,7 @@ import { GridModule } from "@progress/kendo-angular-grid";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BalanceComponent } from "./balance/balance.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { TooltipModule } from '@progress/kendo-angular-tooltip';
-import { AutoClassifierComponent } from './auto-classifier/auto-classifier.component';
-
+import { TooltipModule } from "@progress/kendo-angular-tooltip";
 
 @NgModule({
   declarations: [
@@ -24,7 +22,6 @@ import { AutoClassifierComponent } from './auto-classifier/auto-classifier.compo
     HomeComponent,
     ChartComponent,
     BalanceComponent,
-    AutoClassifierComponent,
   ],
   imports: [
     BrowserModule,
