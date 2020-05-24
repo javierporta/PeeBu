9fc57dcf9e3a99178b79769fba24f471742ca33d
Javier <2190271@my.ipleiria.pt>
2020-05-24 10:32:00 +0100

    Inject transactions into chart component

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionsService } from '../services/transactions.service';
+import { TransactionModel } from '../models/transaction-model';
 
 @Component({
   selector: 'app-chart',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
+  transactions: TransactionModel[];
 
-  constructor() { }
+  constructor(private transactionService: TransactionsService) { }
 
   ngOnInit() {
+    this.transactionService.transactions.subscribe(
+      (result) => this.onGetTransactionsSuccess(result),
+      (error) => this.onGetTransactionsError(error)
+    );
   }
+
+  onGetTransactionsSuccess(result: TransactionModel[]) {
+    this.transactions = result;
+  }
+
+  onGetTransactionsError(error) { }
 
 }
